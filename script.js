a48d7c01a170740e6d8f47370a1dac2b5d385e67
Harsh Natuskar <74592384+Harshnatuskar@users.noreply.github.com>
2023-10-09 16:37:18 +0530

    Update script.js
    The chat configuration class was outdated

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-import { config } from "dotenv"
+import { config } from 'dotenv' 
 config()
 
-import { Configuration, OpenAIApi } from "openai"
+import { OpenAI } from 'openai'
 import readline from "readline"
 
-const openAi = new OpenAIApi(
-  new Configuration({
-    apiKey: process.env.OPEN_AI_API_KEY,
-  })
-)
+const openai = new OpenAI( { apiKey: process.env.API_KEY } );
 
+ 
 const userInterface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 })
 
 userInterface.prompt()
-userInterface.on("line", async input => {
-  const response = await openAi.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: input }],
-  })
-  console.log(response.data.choices[0].message.content)
-  userInterface.prompt()
+userInterface.on("line",async input=>{
+    const response = await openai.chat.completions.create({
+        model:"gpt-3.5-turbo",
+        messages: [
+            { role: "user", content: input }
+        ]
+    })
+    console.log(response.data.choices[0].message.content)
+    userInterface.prompt()
 })
